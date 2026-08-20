fff627fa9c2ff4829fa200aad4ecff777eebc51b
artemdolzhanskiy <dolzhanskiy2008@gmail.com>
2026-08-20 15:53:03 +0300

    second version sq_ur

--- main.c
+++ main.c
@@ -1,36 +1,75 @@
 #include <stdio.h>
 #include <math.h>
+#include <assert.h>
 
+
+void priv(double a, double b, double c);
+void poka(double x1, double x2);
+void a_0(double a, double b, double c);
+void b_0(double a, double b, double c);
+
+void umn_vvod(double a, double b, double c);
 double min_root_kv_ur(double a, double b, double c);
 double max_root_kv_ur(double a, double b, double c);
 double descriminant(double a, double b, double c);
+
 
 int main(){
     printf("Hello! This program solve kv_ur. \nEnter input data: a, b, c: \n");
 
     double a = 0, b = 0, c = 0;
     scanf("%lf %lf %lf", &a, &b, &c);
+    //umn_vvod(double &a, double &b, double &c);
+    
+
+    printf("%lf %lf %lf \n", a, b, c);
+
+    if (a == 0 && b != 0){
+        a_0(a, b, c);
+        exit(0);
+    }else if (a == 0 && b == 0){
+        printf("This equation has dohuya roots.\n");
+        exit(0);
+    }
+
+
+    priv(a, b, c);
+
+    if (descriminant(a, b, c) < 0){
+        exit(0);
+    }
 
     double x1 = 0, x2 = 0;
+    x1 = min_root_kv_ur(a, b, c);
+    x2 = max_root_kv_ur(a, b, c);
+
+    poka(x1, x2);
+
+    return 0;
+}
 
 
+
+
+
+void priv(double a, double b, double c){
     if (descriminant(a, b, c) == 0)
         printf("This equation has 1 root.\n");
     else if (descriminant(a, b, c) > 0)
         printf("This equation has 2 roots.\n");
     else
         printf("This equation hasn't roots.\n");
-
-        
-    if (descriminant(a, b, c) < 0){
-        exit(0);
-    }
-
-    
-    x1 = min_root_kv_ur(a, b, c);
-    x2 = max_root_kv_ur(a, b, c);
+}
 
 
+void a_0(double a, double b, double c){
+    printf("equation is liner, because a = 0.\n");
+    double res3 = (-c) / b;
+    printf("equation has 1 root %lf\n", res3);
+}
+
+
+void poka(double x1, double x2){
     if (x1 == x2){
         printf("root %lf \n", x1);
     }
@@ -38,15 +77,53 @@ int main(){
         printf("min_root %lf \n", x1);
         printf("max_root %lf \n", x2);
     }
-
-    return 0;
 }
+
+
+/*void umn_vvod(double a, double b, double c){
+    char str[100];
+    fgets(str, sizeof(str), stdin);
+    int V = str[0];
+    while
+    
+}*/
+
+
+/*double umn_vvod(){
+    double chislo = 0;
+    int V;
+    V = getchar();
+    while(V == ' ' || V == '\n'){
+        if (V == '1'){
+            chislo = chislo * 10 + 1; 
+        }else if (V == '2'){
+            chislo = chislo * 10 + 2; 
+        }else if (V == '3'){
+            chislo = chislo * 10 + 3; 
+        }else if (V == '4'){
+            chislo = chislo * 10 + 4; 
+        }else if (V == '5'){
+            chislo = chislo * 10 + 5; 
+        }else if (V == '6'){
+            chislo = chislo * 10 + 6; 
+        }else if (V == '7'){
+            chislo = chislo * 10 + 7; 
+        }else if (V == '8'){
+            chislo = chislo * 10 + 8; 
+        }else if (V == '9'){
+            chislo = chislo * 10 + 9; 
+        }else if (V == '0'){
+            chislo = chislo * 10 + 0;
+        }
+        V = getchar();
+    }
+    return chislo;
+}*/
 
 
 double descriminant(double a, double b, double c){
     double Descr = (b * b - 4 * a * c);
     return Descr;
-
 }
 
 
